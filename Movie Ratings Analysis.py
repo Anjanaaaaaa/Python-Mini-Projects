c78184cba96a53d802c4ebf8fc47c5fa61bb87a0
Anjanaaaaaa <87217913+Anjanaaaaaa@users.noreply.github.com>
2025-08-15 20:19:28 +0530

    Update Movie Ratings Analysis.py

--- Movie Ratings Analysis.py
+++ Movie Ratings Analysis.py
@@ -3,6 +3,15 @@
 import pandas as pd
 import matplotlib as plt
 
+# Replace with your share link
+url = 'https://drive.google.com/file/d/1VYf9nra3vFRFSKfwPu-jtrVfBXvfy3xr/view?usp=drive_link'
+
+# Convert to direct download link
+file_id = url.split('/d/')[1].split('/')[0]
+download_url = f'https://drive.google.com/uc?id=movies'
+
+df = pd.read_csv(download_url)
+
 # Load the dataset (replace 'movies.csv' with your actual file name)
 df = pd.read_csv("movies.csv")
 
